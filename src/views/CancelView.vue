34c46603ed075d9e1c71e93daf10cc8818683e23
azat44 <160567934+azat44@users.noreply.github.com>
2025-07-25 20:43:52 +0200

    Update CancelView.vue

--- src/views/CancelView.vue
+++ src/views/CancelView.vue
@@ -1,15 +1,1 @@
-<template>
-  <div class="flex flex-col items-center justify-center h-screen bg-red-100 text-center">
-    <h1 class="text-3xl font-bold text-red-700 mb-4">❌ Paiement annulé</h1>
-    <p class="text-lg text-red-800 mb-6">Votre commande n’a pas été finalisée.</p>
-    <a :href="ksmUrl">
-      <button class="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700">
-        Retour à l'accueil
-      </button>
-    </a>
-  </div>
-</template>
 
-<script setup>
-  const ksmUrl = import.meta.env.KSM_URL
-</script>
